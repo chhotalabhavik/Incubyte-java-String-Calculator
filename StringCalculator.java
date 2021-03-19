0f39f5f11a45792c38e7536146a58e2c46c87659
chhotalabhavik <bhavikchhotala@gmail.com>
2021-03-19 23:48:29 +0530

    simple sum function added

--- StringCalculator.java
+++ StringCalculator.java
@@ -3,5 +3,17 @@
 public class StringCalculator {
 
 	
+	private String delimiter;
+	private String numbers;
+
+	private StringCalculator(String delimiter, String numbers) {
+		this.delimiter = delimiter;
+		this.numbers = numbers;
+	}
+
+	private int sum() {
+		
+		return getNumbers().sum();
+	}
 
 }
--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -10,6 +10,15 @@ import static org.junit.Assert.assertThat;
 
 public class StringCalculatorTest {
 
+    @Test
+	public void sumsEmptyStringTo0() {
+		assertThat(StringCalculator.sum(""), is(0));
+	}
 
+	@Test
+	public void sumsSingleNumberToItself() {
+		assertThat(StringCalculator.sum("5"), is(5));
+		assertThat(StringCalculator.sum("42"), is(42));
+	}
 	
 }
